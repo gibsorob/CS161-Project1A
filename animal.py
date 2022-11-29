af1ddf711d75c3fc90c59748c0ff9d21551c3ff2
Nate Gibson <gibsorob@oregonstate.edu>
2022-11-29 12:22:10 -0500

    Testing version control.

--- animal.py
+++ animal.py
@@ -1,7 +1,7 @@
 # Author: Tim Alcon
 # Date: 1/1/1970
 # Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
+#              prints out that that is their favorite animal. Testing version control
 
 
 fave_animal = input("Please enter your favorite animal: ")
